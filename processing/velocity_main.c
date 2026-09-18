3d9fddfb471049fcb170b7984f6dca1e80bfc24f
Matteo Golin <matteo.golin@gmail.com>
2026-09-18 09:55:42 -0400

    feat: velocity take cmd line arguments

--- processing/velocity_main.c
+++ processing/velocity_main.c
@@ -4,7 +4,9 @@
 
 #include <nuttx/config.h>
 
+#include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include <math.h>
 #include <poll.h>
 #include <stdint.h>
@@ -28,6 +30,10 @@
 
 #define US_TO_S (1e-06)
 
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
 /* Program already knows about altitude and velocity data */
 
 ORB_DECLARE(fusion_altitude);
@@ -45,14 +51,26 @@ ORB_DECLARE(sensor_velocity);
  * Private Functions
  ****************************************************************************/
 
-static struct sensor_velocity vel_from_alt(struct fusion_altitude *prev,
-                                           struct fusion_altitude *cur)
+/****************************************************************************
+ * Name: vel_from_alt
+ *
+ * Description:
+ *   Calculate velocity discretely using two altitude measurements.
+ *
+ * Input Parameters:
+ *   prev - The altitude measurement from the previous time step
+ *   cur - The current altitude measurement
+ *   res - Where to store the resulting velocity measurement
+ *
+ ****************************************************************************/
+
+static void calculate_vel(struct fusion_altitude *prev,
+                          struct fusion_altitude *cur,
+                          struct sensor_velocity *res)
 {
-  return (struct sensor_velocity){
-      .timestamp = prev->timestamp,
-      .velocity = (cur->altitude - prev->altitude) /
-                  ((float)(cur->timestamp - prev->timestamp) * US_TO_S),
-  };
+  res->timestamp = cur->timestamp;
+  res->velocity = (cur->altitude - prev->altitude) /
+                  ((float)(cur->timestamp - prev->timestamp) * US_TO_S);
 }
 
 /****************************************************************************
@@ -61,45 +79,81 @@ static struct sensor_velocity vel_from_alt(struct fusion_altitude *prev,
 
 int main(int argc, char **argv)
 {
+  int c;
   int err;
   int vel_fd;
-  int alt_fd;
+  int devno = 0;
+  int alt_devno = 0;
   float prev_vel = 0.0f;
   struct sensor_velocity vel_data;
   struct fusion_altitude prev_alt;
   struct fusion_altitude cur_alt;
   struct pollfd pfd;
 
+  while ((c = getopt(argc, argv, ":n:a:")) != -1)
+    {
+      switch (c)
+        {
+        case 'n':
+          devno = atoi(optarg);
+          break;
+
+        case 'a':
+          alt_devno = atoi(optarg);
+          break;
+
+        case ':':
+          syslog(LOG_ERR | LOG_USER, "Option -%c requires an argument.\n",
+                 optopt);
+          return EXIT_FAILURE;
+
+        case '?':
+          syslog(LOG_ERR | LOG_USER, "Unknown option '-%c'.\n", optopt);
+          break; /* Don't exit, parse other options */
+
+        default:
+          syslog(LOG_ERR | LOG_USER, "Usage: velocity_fusion [-n devno]\n");
+          return EXIT_FAILURE;
+        }
+    }
+
   /* Set up velocity fusion topic for publishing */
 
-  vel_fd =
-      orb_advertise_multi_queue(ORB_ID(sensor_velocity), NULL, NULL,
-                                CONFIG_ROCKETALT_VELFUSION_QLEN);
+  vel_fd = orb_advertise_multi_queue(ORB_ID(sensor_velocity), NULL, &devno,
+                                     CONFIG_ROCKETALT_VELFUSION_QLEN);
   if (vel_fd < 0)
     {
       syslog(LOG_ERR | LOG_USER,
-             "Could not advertise sensor_velocity topic: %d\n", errno);
+             "Could not advertise sensor_velocity%d topic: %d\n", devno,
+             errno);
       return EXIT_FAILURE;
     }
 
-  syslog(LOG_INFO | LOG_USER, "sensor_velocity topic advertised.\n");
+  syslog(LOG_INFO | LOG_USER, "sensor_velocity%d advertised.\n", devno);
 
-  /* Subscribe to first altitude topic */
+  /* Subscribe to altitude topic */
 
-  alt_fd = orb_subscribe_multi(ORB_ID(fusion_altitude), 0);
-  if (alt_fd < 0)
+  pfd.fd = orb_subscribe_multi(ORB_ID(fusion_altitude), alt_devno);
+  if (pfd.fd < 0)
     {
       syslog(LOG_ERR | LOG_USER,
-             "Could not subscribe to fusion_altitude0: %d\n", errno);
+             "Could not subscribe to fusion_altitude%d: %d\n", alt_devno,
+             errno);
       orb_unadvertise(vel_fd);
       return EXIT_FAILURE;
     }
 
   /* Set up polling */
 
-  pfd.fd = alt_fd;
   pfd.events = POLLIN;
   pfd.revents = 0;
+
+  /* Our previous altitude measurement for the first iteration is marked with
+   * a 0 timestamp. This tells us to copy the current measurement and skip an
+   * iteration.
+   */
+
+  prev_alt.timestamp = 0;
 
   /* Forever convert altitude data to velocity data */
 
@@ -112,7 +166,7 @@ int main(int argc, char **argv)
           continue; /* Try again */
         }
 
-      err = orb_copy(ORB_ID(fusion_altitude), alt_fd, &cur_alt);
+      err = orb_copy(ORB_ID(fusion_altitude), pfd.fd, &cur_alt);
       if (err)
         {
           if (errno != ENODATA)
@@ -123,9 +177,15 @@ int main(int argc, char **argv)
           continue;
         }
 
+      if (prev_alt.timestamp == 0)
+        {
+          prev_alt = cur_alt;
+          continue; /* Skip this iteration until we get 1 more measurement */
+        }
+
       /* Calculate raw velocity */
 
-      vel_data = vel_from_alt(&prev_alt, &cur_alt);
+      calculate_vel(&prev_alt, &cur_alt, &vel_data);
       prev_alt = cur_alt;
 
       /* Low pass filtering */
@@ -143,6 +203,6 @@ int main(int argc, char **argv)
     }
 
   orb_unadvertise(vel_fd);
-  orb_unsubscribe(alt_fd);
+  orb_unsubscribe(pfd.fd);
   return EXIT_FAILURE;
 }
