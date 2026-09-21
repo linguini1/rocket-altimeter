8686649fa9d2d302507dc9193121a75b35a109fa
Matteo Golin <matteo.golin@gmail.com>
2026-09-21 17:11:18 -0400

    chore: give processing programs separate directories

--- processing/batmon/batmon_main.c
+++ processing/batmon/batmon_main.c
@@ -18,7 +18,7 @@
 
 #include <uORB/uORB.h>
 
-#include "../common/common.h"
+#include "../../common/common.h"
 
 /****************************************************************************
  * Pre-processor Definitions
--- processing/velocity/velocity_main.c
+++ processing/velocity/velocity_main.c
@@ -16,7 +16,7 @@
 
 #include <uORB/uORB.h>
 
-#include "../common/common.h"
+#include "../../common/common.h"
 
 /****************************************************************************
  * Pre-processor Definitions
