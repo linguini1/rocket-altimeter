4a79971a621221a4dc764e785efeb1cbd9355d6b
Matteo Golin <matteo.golin@gmail.com>
2026-09-22 16:54:19 -0400

    feat: continuity monitor uses sensor_voltage input

--- processing/continuity/continuity_main.c
+++ processing/continuity/continuity_main.c
@@ -21,6 +21,7 @@
 #include <uORB/uORB.h>
 
 #include "sensor/continuity.h"
+#include <sensor/voltage.h>
 
 #include "../../common/common.h"
 
@@ -28,35 +29,20 @@
  * Pre-processor Definitions
  ****************************************************************************/
 
-/* ADC measurement conversion */
-
-#define measure_to_volts(r)                                                  \
-  ((r) * CONFIG_ROCKETALT_CONTMON_VMAX /                                     \
-   CONFIG_ROCKETALT_CONTMON_ADCRESOLUTION)
-
 /****************************************************************************
  * Private Types
  ****************************************************************************/
 
 struct chan_s
 {
-  char *path;     /* Path to ADC device */
-  int fd;         /* File descriptor to the ADC device */
-  uint8_t id;     /* ID of this channel */
-  uint8_t channo; /* Channel number of the ADC device for this channel */
+  int fd;     /* File descriptor to the voltage topic */
+  int devno;  /* Voltage topic device number */
+  uint8_t id; /* Channel ID (for continuity channel) */
 };
 
 /****************************************************************************
  * Private Data
  ****************************************************************************/
-
-/* Channels to measure the continuity of */
-
-static struct chan_s g_channels[CONFIG_ROCKETALT_CONTMON_NUMCHANS];
-
-/* List of file descriptor structures for us to use when polling */
-
-static struct pollfd g_fds[CONFIG_ROCKETALT_CONTMON_NUMCHANS];
 
 /****************************************************************************
  * Private Function Prototypes
@@ -67,140 +53,60 @@ static struct pollfd g_fds[CONFIG_ROCKETALT_CONTMON_NUMCHANS];
  ****************************************************************************/
 
 /****************************************************************************
- * Name: channel_deinit
- *
- * Description:
- *   De-initialize a channel being monitored for continuity.
- *
- * Input Parameters:
- *   chan - The channel being monitored for continuity
- *
- * Returned Value:
- *   0 on success, error code on failure.
- *
- ****************************************************************************/
-
-static int channel_deinit(struct chan_s *chan)
-{
-  int err = 0;
-
-  if (chan->fd < 0) return err; /* Can't close an invalid fd */
-
-  err = close(chan->fd);
-  if (err < 0)
-    {
-      syslog(LOG_ERR | LOG_USER, "Couldn't close channel %d: %d\n", chan->id,
-             errno);
-    }
-
-  return err;
-}
-
-#ifdef CONFIG_ROCKETALT_CONTMON_SWTRIG
-/****************************************************************************
- * Name: channel_trigger
- *
- * Description:
- *   Executes a software trigger for the ADC associated with the channel.
- *
- * Input Parameters:
- *   chan - The channel to trigger a measurement for
- *
- * Returned Value:
- *   0 on success, error code on failure.
- *
- ****************************************************************************/
-
-static int channel_trigger(struct chan_s *chan)
-{
-  int err;
-
-  err = ioctl(chan->fd, ANIOC_TRIGGER, 0);
-  if (err < 0)
-    {
-      syslog(LOG_ERR | LOG_USER,
-             "ADC trigger ioctl failed for channel %d: %d\n", chan->id,
-             errno);
-    }
-
-  return err;
-}
-#endif /* CONFIG_ROCKETALT_CONTMON_SWTRIG */
-
-/****************************************************************************
  * Name: channel_read
  *
  * Description:
- *   Reads the ADC value for this channel.
+ *   Read voltage data tied to a specific continuity channel.
  *
  * Input Parameters:
- *   chan - The channel to read the value of
- *   data - A pointer to where to store the data that was read
+ *   chan - The channel to read voltage data for
+ *   data - Where to store the voltage data
  *
  * Returned Value:
  *   0 on success, error code on failure.
  *
  ****************************************************************************/
 
-static int channel_read(struct chan_s *chan, struct adc_msg_s *data)
+static int channel_read(const struct chan_s *chan,
+                        struct sensor_voltage *data)
 {
   int err = 0;
-  ssize_t bread;
 
-  /* TODO: I only want data from the associated `channo` */
-
-  bread = read(chan->fd, data, sizeof(*data));
-  if (bread < 0)
+  err = orb_copy(ORB_ID(sensor_voltage), chan->fd, data);
+  if (err)
     {
-      syslog(LOG_ERR | LOG_USER, "Failed to read channel %d: %d\n", chan->id,
-             errno);
-      return errno;
-    }
-  else if (bread == 0)
-    {
-      syslog(LOG_ERR | LOG_USER, "Failed to read channel %d: no data\n",
-             chan->id);
-      return EAGAIN;
+      syslog(LOG_ERR | LOG_USER, "Couldn't get sensor_voltage%d data: %d\n",
+             chan->devno, errno);
+      err = errno;
     }
 
   return err;
 }
 
 /****************************************************************************
- * Name: publish_continuity
+ * Name: channel_publish
  *
  * Description:
- *   Publishes sensor_continuity data for the passed channel and ADC
- *   measurement.
+ *   Publish continuity data.
  *
  * Input Parameters:
- *   fd - The file descriptor for the continuity topic
- *   chan - The channel associated with this measurement
- *   data - The voltage measurement from the ADC
+ *   chan - The file descriptor of the continuity topic
+ *   data - The continuity data to publish
  *
  * Returned Value:
  *   0 on success, error code on failure.
  *
  ****************************************************************************/
 
-static int publish_continuity(int fd, struct chan_s *chan,
-                              struct adc_msg_s *data)
+static int continuity_publish(int fd, const struct sensor_continuity *data)
 {
   int err;
-  struct sensor_continuity cont;
-  int32_t voltage;
 
-  cont.timestamp = orb_absolute_time();
-  voltage = measure_to_volts(data->am_data);
-  cont.continuous = voltage >= CONFIG_ROCKETALT_CONTMON_VTHRESH ? 1 : 0;
-  cont.id = chan->id;
-
-  err = orb_publish(ORB_ID(sensor_continuity), fd, &cont);
+  err = orb_publish(ORB_ID(sensor_continuity), fd, data);
   if (err)
     {
-      syslog(LOG_ERR | LOG_USER,
-             "Couldn't publish continuity for channel %d: %d\n", chan->id,
-             errno);
+      syslog(LOG_ERR | LOG_USER, "Couldn't publish continuity: %d\n", errno);
+      err = errno;
     }
 
   return err;
@@ -216,17 +122,31 @@ int main(int argc, char **argv)
   int err;
   int ret = EXIT_FAILURE;
   int cont_fd;
-  int devno = 0;
-  struct adc_msg_s adc_data;
+  float v_thresh;
+  int num_chans;
+  struct pollfd *pfds = NULL;
+  struct chan_s *channels = NULL;
+  struct sensor_voltage voltage;
+  struct sensor_continuity continuity;
+
+  int devno = 0;           /* Device number for continuity topic */
+  uint8_t channo_base = 0; /* Channel ID base */
 
   /* Parse command line arguments */
 
-  while ((c = getopt(argc, argv, ":n:")) != -1)
+  while ((c = getopt(argc, argv, ":n:c:")) != -1)
     {
       switch (c)
         {
         case 'n':
-          devno = atoi(optarg);
+          devno = atoi(optarg); /* Device number for continuity topic */
+          break;
+
+        case 'c':
+          /* Base number at which to start enumerating continuity channel IDs
+           */
+
+          channo_base = strtoul(optarg, NULL, 0);
           break;
 
         case ':':
@@ -239,132 +159,137 @@ int main(int argc, char **argv)
           break; /* Don't exit, parse other options */
 
         default:
-          syslog(LOG_ERR | LOG_USER,
-                 "Usage: continuity [-n devno] devpath channo ...\n");
+          syslog(LOG_ERR | LOG_USER, "Usage: continuity [-n devno] [-c "
+                                     "channo] v_thresh topicno [...]\n");
           return EXIT_FAILURE;
         }
     }
 
-  /* Ensure enough arguments were passed to the program for all of the ADC
-   * device paths and channel numbers.
-   */
+  /* Check for threshold voltage */
 
-  if (argc - optind < 2 * CONFIG_ROCKETALT_CONTMON_NUMCHANS)
+  if (argc <= optind)
     {
-      syslog(LOG_ERR | LOG_USER,
-             "Not enough positional arguments for all channels. Got %d, need "
-             "%d\n",
-             argc - optind, 2 * CONFIG_ROCKETALT_CONTMON_NUMCHANS);
+      syslog(LOG_ERR | LOG_USER, "Expected v_thresh argument.\n");
       return EXIT_FAILURE;
     }
 
-  /* Initialize channels and pollfds */
+  v_thresh = strtof(argv[optind], NULL);
+  optind++;
 
-  for (int i = 0; i < array_len(g_channels); i++)
+  /* Verify we have some actual inputs to work with */
+
+  num_chans = argc - optind;
+  if (num_chans < 1)
     {
-      g_channels[i].id = i + 1;
-      g_channels[i].fd = -1;
-      g_channels[i].path = argv[optind];
-      g_channels[i].channo = atoi(argv[optind + 1]);
-      optind += 2;
+      syslog(LOG_ERR | LOG_USER,
+             "Expected at least one sensor_voltage topic as input.\n");
+      return EXIT_FAILURE;
+    }
 
-      syslog(LOG_INFO | LOG_USER, "Channel %d using %s, ADC channo %d\n",
-             g_channels[i].id, g_channels[i].path, g_channels[i].channo);
+  /* Allocate structures for each input voltage */
 
-      g_fds[i].fd = -1;
-      g_fds[i].events = POLLIN;
-      g_fds[i].revents = 0;
+  pfds = malloc(num_chans * sizeof(struct pollfd));
+  if (pfds == NULL)
+    {
+      syslog(LOG_ERR | LOG_USER, "Couldn't allocate pollfd structures: %d\n",
+             errno);
+      return EXIT_FAILURE;
+    }
+
+  channels = malloc(num_chans * sizeof(struct chan_s));
+  if (channels == NULL)
+    {
+      syslog(LOG_ERR | LOG_USER, "Couldn't allocate channel structures: %d\n",
+             errno);
+      ret = EXIT_FAILURE;
+      goto free_pfds;
+    }
+
+  /* Initialize structures */
+
+  for (int i = 0; i < num_chans; i++, optind++)
+    {
+      pfds[i].revents = 0;
+      pfds[i].events = POLLIN;
+      channels[i].id = channo_base + i;
+      channels[i].devno = strtoul(argv[optind], NULL, 10);
+
+      pfds[i].fd =
+          orb_subscribe_multi(ORB_ID(sensor_voltage), channels[i].devno);
+      if (pfds[i].fd < 0)
+        {
+          syslog(LOG_ERR | LOG_USER,
+                 "Couldn't subscribe to sensor_voltage%d\n",
+                 channels[i].devno);
+          ret = EXIT_FAILURE;
+          goto clean_channels;
+        }
+
+      channels[i].fd = pfds[i].fd;
     }
 
   /* Set up continuity topic for publishing */
 
   cont_fd = orb_advertise_multi_queue(ORB_ID(sensor_continuity), NULL, &devno,
-                                      CONFIG_ROCKETALT_CONTMON_VOLTAGE_QLEN);
+                                      CONFIG_ROCKETALT_CONTMON_QLEN);
   if (cont_fd < 0)
     {
       syslog(LOG_ERR | LOG_USER,
              "Could not advertise sensor_continuity%d: %d\n", devno, errno);
-      return EXIT_FAILURE;
+      ret = EXIT_FAILURE;
+      goto clean_channels;
     }
 
   syslog(LOG_INFO | LOG_USER, "sensor_continuity%d advertised.\n", devno);
 
-  /* Open ADC devices */
-
-  for (int i = 0; i < array_len(g_channels); i++)
-    {
-      err = open(g_channels[i].path, O_RDONLY);
-      if (err < 0)
-        {
-          syslog(LOG_ERR | LOG_USER, "Could not open %s: %d\n",
-                 g_channels[i].path, errno);
-          ret = EXIT_FAILURE;
-          goto cleanup_channels;
-        }
-
-      g_channels[i].fd = err; /* Store resulting fd */
-      g_fds[i].fd = err;
-    }
-
-  /* Forever convert ADC measurements to voltage uORB output */
+  /* Forever convert voltages to continuity uORB output */
 
   for (;;)
     {
-      /* For each channel, trigger an ADC measurement if that's necessary
-       * (i.e. software trigger required). Nothing we can do if this fails.
-       */
-#ifdef CONFIG_ROCKETALT_CONTMON_SWTRIG
-      for (int i = 0; i < array_len(g_channels); i++)
-        {
-          channel_trigger(&g_channels[i]);
-        }
-#endif /* CONFIG_ROCKETALT_CONTMON_SWTRIG */
-
-      /* Now we poll on the file descriptors for each channel until something
-       * is ready to handle.
-       */
-
-      err = poll(g_fds, array_len(g_fds), 0);
+      err = poll(pfds, num_chans, -1);
       if (err <= 0)
         {
           syslog(LOG_ERR | LOG_USER, "Failed to poll: %d\n", errno);
           continue; /* Try again */
         }
 
-      for (int i = 0; i < array_len(g_fds); i++)
+      for (int i = 0; i < num_chans; i++)
         {
-          if (g_fds[i].revents & POLLIN)
+          if (pfds[i].revents & POLLIN)
             {
-              /* We have data. Read the channel and publish the result if the
+              /* We have data. Read the topic and publish the result if the
                * read succeeds.
                */
 
-              err = channel_read(&g_channels[i], &adc_data);
+              err = channel_read(&channels[i], &voltage);
               if (err == 0)
                 {
-                  publish_continuity(cont_fd, &g_channels[i], &adc_data);
+                  continuity.timestamp = voltage.timestamp;
+                  continuity.id = channels[i].id;
+                  continuity.continuous = voltage.voltage > v_thresh ? 1 : 0;
+                  continuity_publish(cont_fd, &continuity);
                 }
 
-              g_fds[i].revents = 0; /* Clear the events for next time */
+              pfds[i].revents = 0; /* Clear the events for next time */
             }
         }
-
-#ifdef CONFIG_ROCKETALT_CONTMON_SWTRIG
-      /* If we're manually triggering the ADC, make sure we only read the ADCs
-       * often as the user configured.
-       */
-
-      sleep(CONFIG_ROCKETALT_CONTMON_PERIOD);
-#endif /* CONFIG_ROCKETALT_CONTMON_SWTRIG */
-    }
-
-cleanup_channels:
-  for (int i = 0; i < array_len(g_channels); i++)
-    {
-      channel_deinit(&g_channels[i]);
     }
 
   orb_unadvertise(cont_fd);
+
+clean_channels:
+  for (int i = 0; i < num_chans; i++)
+    {
+      if (pfds[i].fd > 0)
+        {
+          orb_unsubscribe(pfds[i].fd);
+        }
+    }
+
+  free(channels);
+
+free_pfds:
+  free(pfds);
 
   return ret;
 }
